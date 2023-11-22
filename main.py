469160ad1f3692110a569c3cfa71fee17d862a82
dariobotas <dario.botas@gmail.com>
2023-11-22 15:11:37 +0000

    STEP2 - START

---
+++ main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import os
+
+
+ACCOUNT_EMAIL = os.environ[]
+ACCOUNT_PASSWORD = os.environ[]
